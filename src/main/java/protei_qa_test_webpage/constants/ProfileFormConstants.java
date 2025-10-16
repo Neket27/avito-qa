52c3cf31cf0ae48628b32dd627af4ca5461e7e46
neket <nikitaivanovitc@gmail.com>
2025-10-16 22:24:45 +0300

    В приложенном html-файле имеется две формы: форма авторизации и анкета, доступная после авторизации. Требуется, используя инструмент Selenium, написать автотесты, которые проверяли бы работу этих форм

---
+++ src/main/java/protei_qa_test_webpage/constants/ProfileFormConstants.java
@@ -0,0 +1,10 @@
+package protei_qa_test_webpage.constants;
+
+public class ProfileFormConstants extends BaseConstants {
+
+    public static final String NAME = "Тест";
+    public static final String GENDER = "Женский";
+    public static final boolean Option11 = true;
+    public static final boolean Option12 = true;
+    public static final String OPTION2 = "2.1";
+}
